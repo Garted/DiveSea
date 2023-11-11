990d518b891d7f7ca3e144b4fe296bfece60334b
garted <garted93@gmail.com>
2023-11-11 23:04:30 +0300

    sdfsd

---
+++ dist/js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const left = document.querySelector(".weekly__left");
+  const right = document.querySelector(".weekly__right");
+  const wrap = document.querySelector(".weekly__wrap");
+
+  right.addEventListener("click", () => {
+    wrap.scrollBy({
+      left: 280,
+      behavior: "smooth",
+    });
+  });
+
+  left.addEventListener("click", () => {
+    wrap.scrollBy({
+      left: -280,
+      behavior: "smooth",
+    });
+  });
+
+  /////Добавляем рандомные проценты в верстку
+
+  const chers = document.querySelectorAll(".recent__redcheers");
+  function randomPercent() {
+    return Number((Math.random() * 100).toFixed(2));
+  }
+  setInterval(() => {
+    chers.forEach((cher) => {
+      cher.innerHTML = `${randomPercent()}%`;
+    });
+  }, 3500);
+});
